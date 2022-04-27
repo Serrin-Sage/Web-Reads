585ec41aa76042b67edd4e26691b54a37051f07f
Sadia Chowdhury <sadiachowdhury2000@gmail.com>
2022-04-27 11:45:00 -0400

    updated signin/up

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -1,29 +1,71 @@
 import { useState } from "react";
 import Validation from "./Validation";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 
 function SignUpForm() {
 
-    const [values, setValues] = useState({
-        fullname:"",
-        email:"",
-        password:""
-    });
+    // const [values, setValues] = useState({
+    //     fullname:"",
+    //     email:"",
+    //     password:"",
+    //     confirmpassword: "",
+    // });
+    //usestates for all the fields 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmpassword, setConfirmpassword] = useState("");
+    const [errormessage, setErrormessage] = useState("");
 
+    //error 
+    
     const [errors, setErrors] = useState({});
 
-    const handleChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value,
-        })
-    }
+    // const handleChange = (e) => {
+    //     setValues({
+    //         ...values,
+    //         [e.target.name]: e.target.value,
+    //     })
+    // }
 
-    const handleFormSubmit = (e) => {
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
-        setErrors(Validation(values));
-    }
+        // setErrors(Validation(values));
+        //console.log(values.fullname);
+
+        //check if the password is same with the confirmed password
+        if(password !== confirmpassword){
+            setErrormessage("Passwords do NOT match");
+        }
+        else{
+            setErrormessage("");
+            try {
+                const config = {
+                    headers:{
+                        "Content-type" : "application/json"
+                    }
+                }
+
+                const { data } = await axios.post(
+                    "/api/users/signup",
+                    {
+                        name,
+                        email,
+                        password
+                    },
+                    config
+                );
+                console.log(data);
+                localStorage.setItem("userInfo", JSON.stringify(data));
+                
+            } catch (error) {
+                setErrors(error.response.data.message);
+            }
+        }
+
+
+    };
     
     return (
         <div className="formcontainer">
@@ -33,14 +75,14 @@ function SignUpForm() {
                 </div>
                 <form className="form-wrapper">
                     <div className="form-field">
-                        <label>Full Name</label>
+                        <label>Name</label>
                         <br></br>
                         <input 
                             className="input" 
                             type="text" 
-                            name="fullname" 
-                            value={values.fullname} 
-                            onChange={handleChange} 
+                            name="name" 
+                            value={name} 
+                            onChange={(e) => setName(e.target.value)} 
                         />
                         {errors.fullname && <p className="error">{errors.fullname}</p>}
                     </div>
@@ -51,8 +93,8 @@ function SignUpForm() {
                             className="input" 
                             type="text" 
                             name="email" 
-                            value={values.email}
-                            onChange={handleChange}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                         {errors.email && <p className="error">{errors.email}</p>}
                     </div>
@@ -63,8 +105,8 @@ function SignUpForm() {
                             className="input" 
                             type="password" 
                             name="password" 
-                            value={values.password}
-                            onChange={handleChange}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                         {errors.password && <p className="error">{errors.password}</p>}
                     </div>
@@ -74,18 +116,19 @@ function SignUpForm() {
                         <input 
                             className="input" 
                             type="password" 
-                            name="password" 
-                            value={values.password}
-                            onChange={handleChange}
+                            name="confirmpassword" 
+                            value={confirmpassword}
+                            onChange={(e) => setConfirmpassword(e.target.value)}
                         />
                         {errors.password && <p className="error">{errors.password}</p>}
                     </div>
                     <div>
                         <button className="submit" onClick={handleFormSubmit}>Sign up</button>
                     </div>
+                    {errormessage}
                 </form>
                 <div>
-                <p className="bottom-text">Already have an account? Sign in <Link to='/signin' className="here-text">
+                <p>Already have an account? Sign in <Link to='/signin' >
                 here!</Link>
                 </p>
                 </div>

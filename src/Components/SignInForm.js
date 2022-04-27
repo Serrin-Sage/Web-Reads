ab13af0dbbf522cda6caac9497482ebf6f5e7055
Sadia Chowdhury <sadiachowdhury2000@gmail.com>
2022-04-27 10:36:18 -0400

    signin/up functionality

--- src/Components/SignInForm.js
+++ src/Components/SignInForm.js
@@ -1,28 +1,60 @@
 import { useState } from "react";
-import Validation from "./Validation";
+// import Validation from "./Validation";
 import { Link } from "react-router-dom";
-
+import axios from "axios"
 
 function SignInForm() {
 
-    const [values, setValues] = useState({
-        email:"",
-        password:""
-    });
+    // const [values, setValues] = useState({
+    //     email:"",
+    //     password:""
+    // });
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const [errors, setErrors] = useState({});
 
-    const handleChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value,
-        })
-    }
+   
+    
 
-    const handleFormSubmit = (e) => {
+    // const handleChange = (e) => {
+    //     setValues({
+    //         ...values,
+    //         [e.target.name]: e.target.value,
+    //     })
+    // }
+
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
-        setErrors(Validation(values));
-    }
+        // setErrors(Validation(email));
+        // setErrors(Validation(password));
+        console.log(email, password);
+        try {
+            const config = {
+                headers:{
+                    "Content-type" : "application/json"
+                }
+            }
+            const { data } = await axios.post(
+                "/api/users/signin",
+                {
+                    email,
+                    password,
+                },
+                config
+            );
+            console.log({data});
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            window.location.href = '/userpage';
+            
+        } catch (error) {
+             setErrors(error.response.data.message);
+            
+        }
+    };
+
+  
     
     return (
         <div className="formcontainer">
@@ -30,7 +62,7 @@ function SignInForm() {
                 <div>
                     <h2>Sign In</h2>
                 </div>
-                <form className="form-wrapper">
+                <form  className="form-wrapper">
                     <div className="form-field">
                         <label>Email</label>
                         <br></br>
@@ -38,8 +70,8 @@ function SignInForm() {
                             className="input" 
                             type="text" 
                             name="email" 
-                            value={values.email}
-                            onChange={handleChange}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                         {errors.email && <p className="error">{errors.email}</p>}
                     </div>
@@ -50,8 +82,8 @@ function SignInForm() {
                             className="input" 
                             type="password" 
                             name="password" 
-                            value={values.password}
-                            onChange={handleChange}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                         {errors.password && <p className="error">{errors.password}</p>}
                     </div>

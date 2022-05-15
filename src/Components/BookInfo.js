30514cd1e582bd1047f99091d80e7fb04d078d6a
cbarwicki <cbarwicki14@gmail.com>
2022-05-14 22:55:26 -0400

    survey and book list updates

--- src/Components/BookInfo.js
+++ src/Components/BookInfo.js
@@ -8,8 +8,8 @@ function BookInfo({book}) {
             <h3>{book.title}</h3> <br/>
             {book.author}
         </div>
-        <div className='fav-icon'>
-            <FavoriteBorderIcon fontSize='large' />
+        <div className='fav-icon-container'>
+            <FavoriteBorderIcon className='fav-icon' fontSize='large' onClick={() => console.log("test")} />
         </div>
         
     </div>
